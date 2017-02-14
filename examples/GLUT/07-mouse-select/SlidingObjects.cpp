deb8097e243dfaf3c162ae762917b036a934bb6e
Zach Wells <zawells@umail.ucsb.edu>
2017-02-13 17:34:00 -0800

    added the beginnings of sliding blocks

---
+++ examples/GLUT/07-mouse-select/SlidingObjects.cpp
@@ -0,0 +1,23 @@
+/*	File Name: Objects.cpp
+Author : Zachary Wells
+Date : 2 / 13 / 2017
+Tested With : Chai version 3.1.1
+*/
+#include "graphics/CPrimitives.h"
+#include "SlidingObjects.hpp"
+
+//debug
+#include <iostream>
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//constructor
+ZSlidingObjects::ZSlidingObjects()
+{
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//destructor
+ZSlidingObjects::~ZSlidingObjects()
+{
+}
---
+++ examples/GLUT/07-mouse-select/SlidingObjects.hpp
@@ -0,0 +1,42 @@
+#pragma once
+/*
+
+These are objects that will populate the world
+At the current point they will be stationary objects that will
+
+File Name : WorldMarker.hpp
+Author : Zachary Wells
+Date : 10 / 30 / 2016
+Tested With : Chai version 3.1.1
+*/
+
+#ifndef INCLUDE_Objects
+#define INCLUDE_Objects
+
+#include <math/CVector3d.h>
+#include "world\CMesh.h"
+#include "materials\CMaterial.h"
+#include <vector>
+
+using namespace chai3d;
+
+class ZSlidingObjects
+{
+	//-------------------------------------------------------------------------
+	// PUBLIC METHODS:
+	//-------------------------------------------------------------------------
+public:
+	//constructor
+	ZSlidingObjects();
+
+	//destructor
+	~ZSlidingObjects();
+
+	//-----------------------------------------------------------------------
+	// PUBLIC METHODS:
+	//--------------------------------------------------------------------------
+public:
+
+};
+
+#endif
